230eba7d1e3644d3678a1d37af5f8e26f8d9848c
Erick Bordallo <erickbordallo@Ericks-MacBook-Air.local>
2016-05-11 19:22:02 -0300

    Funcao transformar String em Int adicionada

--- funcoesUteis.swift
+++ funcoesUteis.swift
@@ -1,6 +1,13 @@
-//Função para transformar todas as letras em maiúsculo
 import UIKit
+
+//Função para transformar todas as letras em maiúsculo
 
 func getUpperVersion (str: String) -> String{
     return str.uppercaseString
+}
+
+//Trasformar Tipo String em Integer
+
+func transformTextInInteger(str: String) -> Int{
+    return Int(str)
 }
